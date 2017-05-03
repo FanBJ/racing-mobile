41295ca7d6a983c9560073ec8abec316fcb5aef1
FanBJ <54fbj@163.com>
2017-05-03 18:01:50 +0800

    1

---
+++ src/main/java/com/hy/racing/userinfo/services/imp/UserinfoServicesImp.java
@@ -0,0 +1,26 @@
+package com.hy.racing.userinfo.services.imp;
+
+import com.hy.racing.entity.Userinfo;
+import com.hy.racing.userinfo.services.IUserInfoServices;
+
+public class UserinfoServicesImp implements IUserInfoServices {
+
+	@Override
+	public Userinfo getUserById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Userinfo getUserByOpenid(String openid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int addUserinfo(Userinfo user) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
